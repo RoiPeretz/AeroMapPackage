bc2032c59b35f92a10b80f3a76de3ecc67a853bb
roi.peretz <roi081295@gmail.com>
2022-03-29 16:22:50 +0300

    updated config

--- TinyClient/webpack.config.js
+++ TinyClient/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   plugins:[
         new HtmlWebpackPlugin({
-            template: 'src/index.html'
+          template: "./src/index.html",
         }),
     ],
   resolve: {
