cb88fb8a0ed055dcb347ce4d3b5e8350c5da7f59
roi.peretz <roi081295@gmail.com>
2022-03-31 17:29:31 +0300

    client DI

--- aeroMapClient/src/index.ts
+++ aeroMapClient/src/index.ts
@@ -1,6 +1,12 @@
-import { helloAeroMap } from '../node_modules/aero-map';
+import "reflect-metadata";
+import { Dependency } from './dependency';
+import { clientContainer } from "../inversify.config"
+import { helloAeroMap } from 'aero-map';
 import '../node_modules/aero-map/dist/index.css'
 import './index.css'
 
 console.log("Hello from aero-map client");
 helloAeroMap();
+
+var d = clientContainer.get<Dependency>(Dependency);
+d.bla();
